099749ed3c4d94ac9ee5123bcd239a883d89ded0
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-19 01:42:50 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,14 +2,15 @@ def hello_t(names)
   i = 0 
   
   while i < names.length 
-    yield names[i]
+    yield(names[i])
     i = i + 1 
   end
+  names 
 end
 
 hello_t(names) do |name|
   if name.start_with?("T")
-    puts "Hi, #{names}"
+    puts "Hi, #{name}"
   end 
 end 
 
