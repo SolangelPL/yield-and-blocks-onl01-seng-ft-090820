8ef0e0f1b9ddff5bb01f41a779c3c6de17d5f6a0
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-19 01:33:25 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,15 @@
-def hello_t
-
+def hello_t(greeting)
+  i = 0 
+  
+  while i < greeting.length 
+    yield length[i]
+    i = i + 1 
+  end
 end
 
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end 
+end 
 
